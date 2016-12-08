e54bd4f5be4a466418e0af82a1bdfeeba6bd210a
BriceDenoun <brice.denoun@gmail.com>
2016-12-08 15:22:27 +0100

    Modify architecture of class

--- script/incremental_SAX.py
+++ script/incremental_SAX.py
@@ -6,15 +6,15 @@ from definitions import ZERO_DIVISION_SAFE
 
 class Incremental_SAX:
   """Class gathering all required function to run SAX during an online acquisition of time series
-  We suppose that we already have a certain amount (frame_size) of points"""
-    def __init__(self, frame_size , alphabet_size, nb_pieces):
-        self.window_size = frame_size
+  We assume that we already have a certain amount (frame_size) of points"""
+    def __init__(self, alphabet_size, nb_pieces ,nb_inside_segment,time_serie):
         self.alphabet_size = alphabet_size
         self.nb_subdivision = nb_pieces
-        self.global_mean = 0
-        self.global_variance = 0 + ZERO_DIVISION_SAFE
-        self.global_frequency = 0
-        self.ts = []
+        self.window_size = nb_pieces * nb_inside_segment
+        self.ts = np.asarray(time_serie)
+        self.global_mean = ts.mean(axis = 0)
+        self.global_variance = ts.var(axis = 0) + ZERO_DIVISION_SAFE
+        self.global_frequency = self.window_size
         
     def update_global_mean_variance(self, x):
         new_global_frequency = self.global_frequency + 1
@@ -23,7 +23,7 @@ class Incremental_SAX:
         self.global_mean = new_global_mean
         self.global_frequency = new_global_frequency
 
-    def update_window(self,new_point):
+
         
 
 
