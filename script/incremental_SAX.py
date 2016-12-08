5e74e1b9ecbdff1daf8fe1edf2180b1d7401a08c
BriceDenoun <brice.denoun@gmail.com>
2016-12-08 17:25:11 +0100

    Second version of incremental_SAX

--- script/incremental_SAX.py
+++ script/incremental_SAX.py
@@ -8,18 +8,21 @@ from definitions import ZERO_DIVISION_SAFE
 
 class Incremental_SAX:
   """Class gathering all required function to run SAX during an online acquisition of time series
-  We assume that we already have a certain amount (frame_size) of points"""
+  We assume that we already have a certain amount of points (500)"""
     def __init__(self, alphabet_size, nb_pieces ,nb_inside_segment,time_serie):
         self.alphabet_size = alphabet_size
+        self.alphabet = range(alphabet_size)
         self.nb_subdivision = nb_pieces
         self.sublen = nb_inside_segment
         self.window_size = nb_pieces * self.sublen
-        self.window = np.asarray(time_serie)
+        self.window = np.asarray(time_serie[:self.window_size])
         self.oldest = self.window[0]
-        self.global_mean = ts.mean(axis = 0)
-        self.global_variance = ts.var(axis = 0) + ZERO_DIVISION_SAFE
+        self.global_mean = self.window.mean(axis = 0)
+        self.global_variance = self.window.var(axis = 0) + ZERO_DIVISION_SAFE
         self.global_frequency = self.window_size
         self.subwin_means = np.asarray(map(lambda xs: xs.mean(axis = 0), np.array_split(self.window, self.nb_subdivision)))
+        self.percentile = np.percentile(self.znormalisation(time_serie),np.linspace(1./alphabet_size, 1-1./alphabet_size, alphabet_size-1)*100)
+        self.SAX = np.asarray([(self.alphabet[0] if ts_value < self.percentile[0] else (self.alphabet[-1] if ts_value > self.percentile[-1] else self.alphabet[np.where(self.percentile <= ts_value)[0][-1]+1])) for ts_value in self.znormalisation(self.window)])
 
     def update_window(self,new_point):
         self.window[self.oldest] = new_point
@@ -32,9 +35,10 @@ class Incremental_SAX:
         self.global_frequency = new_global_frequency
 
     def znormalization(self):
-        return (self.window - self.global_mean) / (np.sqrt(self.global_variance)+ZERO_DIVISION_SAFE)
+        self.window = (self.window - self.global_mean) / (np.sqrt(self.global_variance)+ZERO_DIVISION_SAFE)
 
-    def paa_transform(self):
+    def PAA_SAX_transform(self):
+        #local_subwin = np.copy(self.subwin_means)
         for index,mean in enumerate(self.subwin_means) :
             oldest_first = self.oldest + self.sublen * index
             oldest_next = self.oldest + self.sublen * (index + 1)
@@ -42,16 +46,17 @@ class Incremental_SAX:
                 oldest_first -= self.window_size
             if oldest_next > self.window_size :
                 oldest_next -= self.window_size
-            self.subwin_means[index] = (mean*self.sublen - self.window[oldest_first] + self.window[oldest_next + self.sublen]) * 1.0 / self.sublen
+            local_subwin_mean = (mean*self.sublen - self.window[oldest_first] + self.window[oldest_next + self.sublen]) * 1.0 / self.sublen
+            if local_subwin_mean > self.subwin_means[index] :
+                if local _subwin_mean > self.percentile[self.SAX[index] + 1] :
+                    self.SAX[index] = self.alphabet[np.where(self.percentile[self.SAX[index]:] <= local_subwin_mean)[0][-1]+1]
+            elif local_subwin_mean < self.subwin_means[index] :
+                if local _subwin_mean < self.percentile[self.SAX[index] - 1] :
+                     self.SAX[index] = self.alphabet[np.where(self.percentile[:self.SAX[index]] <= local_subwin_mean)[0][-1]+1]
+            self.subwin_means[index] = local_subwin_mean
         self.oldest += 1
         if self.oldest >= self.window_size : 
             self.oldest = 0
 
     def unormalization(self) :
-        return self.ts * np.sqrt(self.global_variance) + self.global_mean
-
-
-        
-
-
-    
+        self.window =  self.window * np.sqrt(self.global_variance) + self.global_mean
