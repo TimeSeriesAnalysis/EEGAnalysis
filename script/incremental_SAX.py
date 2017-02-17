a877b80d7719520f9af03989c6a9cb9c31589dac
Brice Denoun <brice.denoun@gmail.com>
2017-02-17 23:54:19 +0100

    All method of Incremental SAX file have now a good documentation

--- script/incremental_SAX.py
+++ script/incremental_SAX.py
@@ -1,15 +1,16 @@
+# Import already existing modules
 import numpy as np
-import matplotlib.pyplot as plt
-from scipy.stats import norm
+
+# Import code from our own files
 from definitions import ZERO_DIVISION_SAFE
 
 
-# TO DO : CHECK IF WE HAVE A COLUMN ARRAY
+
 
 class Incremental_SAX:
     """
         Class gathering all required function to run SAX during an online acquisition of time series.
-        We assume that we already have a certain amount of points (500). The result of the algorithm is stored
+        We assume that we already have a certain amount of points chosen by the user through two following parameters. The result of the algorithm is stored
         in the attribute called SAX.
         :param alphabet_size : Length of the alphabet which is numeric
         :type alphabet_size : Integer
@@ -20,20 +21,21 @@ class Incremental_SAX:
         :param time_serie : Data to transform
         :type time_serie : Numpy array of time series (floats numbers)
     """
-    def __init__(self, alphabet_size, nb_subwindow , length_subwindow, time_serie):
+    def __init__(self, alphabet_size, nb_subwindow, length_subwindow, time_serie):
         self.alphabet_size = alphabet_size
         self.alphabet = range(alphabet_size)
         self.nb_subwindow = nb_subwindow
         self.len_subwindow = length_subwindow
         self.window_size = self.nb_subwindow * self.len_subwindow                     #The sliding window size is defined as the product of the number of sub-window and its size
-        self.oldest = 0                                                                                                #Define the first point which will be removed of the sliding frame (the oldest one) 
+        self.window = np.asarray(time_serie[:self.window_size])                            #Creation of the sliding window from the parameter containing starting data. Numpy array
+        self.oldest = 0                                                                                                #Define index of the first point removed when updating the sliding frame (the oldest one) 
         self.global_mean = self.window.mean(axis = 0)                                          #Mean of each series contained in the sliding window
         self.global_variance = self.window.var(axis = 0) + ZERO_DIVISION_SAFE  #Variance of each series contained in the sliding window
         self.global_frequency = self.window_size                                                      #Frequency used to normalize the data each time we have a new point
         self.subwin_means = np.asarray(map(lambda xs: xs.mean(axis = 0), np.array_split(self.window, self.nb_subdivision)))         #Numpy array containing the mean of each subwindow 
-        self.percentile = np.percentile((time_serie - time_serie.mean())/time_serie.std(),np.linspace(1./alphabet_size, 1-1./alphabet_size, alphabet_size-1)*100)       #percentiles of the normalized initial data
+        self.percentile = np.percentile((self.window - self.global_mean) / self.global_variance, np.linspace(1. / self.alphabet_size, 1 - 1. / alphabet_size, alphabet_size - 1) * 100)       #percentiles of the normalized initial data
         self.znormalization()
-        self.SAX = np.asarray([(self.alphabet[0] if ts_value < self.percentile[0] else (self.alphabet[-1] if ts_value > self.percentile[-1] else self.alphabet[np.where(self.percentile <= ts_value)[0][-1]+1])) for ts_value in self.window])          #Compute SAX transformation on initial data 
+        self.SAX = np.asarray([(self.alphabet[0] if ts_value < self.percentile[0] else (self.alphabet[-1] if ts_value > self.percentile[-1] else self.alphabet[np.where(self.percentile <= ts_value)[0][-1] + 1])) for ts_value in self.window])          #Compute SAX transformation on initial data 
         self.unormalization()
 
 
@@ -60,18 +62,25 @@ class Incremental_SAX:
 
 
     def znormalization(self):
+        """
+            Normalize the whole window by substracting its own mean and dividing by its own variance.
+        """
         self.window = (self.window - self.global_mean) / (np.sqrt(self.global_variance)+ZERO_DIVISION_SAFE)
 
+
     def PAA_SAX_transform(self):
-        #local_subwin = np.copy(self.subwin_means)
+        """
+            Perform the discretization and store the transformation into SAX attribute.
+        """
         for index,mean in enumerate(self.subwin_means) :
-            oldest_first = self.oldest + self.sublen * index
-            oldest_next = self.oldest + self.sublen * (index + 1)
+            print 
+            oldest_first = self.oldest + self.len_subwindow * index
+            oldest_next = self.oldest + self.len_subwindow * (index + 1)
             if oldest_first >= self.window_size :
                 oldest_first -= self.window_size
             if oldest_next >= self.window_size :
                 oldest_next -= self.window_size
-            local_subwin_mean = (mean*self.sublen - self.window[oldest_first] + self.window[oldest_next]) * 1.0 / self.sublen
+            local_subwin_mean = (mean*self.len_subwindow - self.window[oldest_first] + self.window[oldest_next]) * 1.0 / self.len_subwindow
             if local_subwin_mean > self.subwin_means[index] :
                 if self.SAX[index] > self.percentile[-1]:
                     self.SAX[index] = self.percentile[-1]
@@ -87,15 +96,27 @@ class Incremental_SAX:
         if self.oldest >= self.window_size : 
             self.oldest = 0
 
+
     def unormalization(self) :
+        """
+            Perform the exact opposite of znormalization function and un-normalize sliding frame's content.
+        """
         self.window =  self.window * np.sqrt(self.global_variance) + self.global_mean
 
+
     def run(self, new_point):
+        """
+            Function gathering all required stage to perform a proper incremental SAX transformation.
+            You need to call this function each time you have a new point.
+            :param new_point : New collected point to add
+            :type new_point : Float number
+        """
         self.update_window(new_point)
-        self.update_global_mean_variance(new_point)
+        #self.update_global_mean_variance(new_point)        #useless since the merge of two functions
         self.znormalization()
         self.PAA_SAX_transform()
         self.unormalization()
+
 
 if __name__ == "__main__":
     isax = Incremental_SAX(10,10,10,np.random.randn(500))
