049783a048273dfecbdd51aa98db90ce8784cedf
Brice Denoun <brice.denoun@gmail.com>
2017-03-19 23:59:30 +0100

    add doc and percentils corrected

--- script/dynamic_SAX.py
+++ script/dynamic_SAX.py
@@ -33,7 +33,7 @@ class Dynamic_SAX:
         self.index_oldest = 0                                                                                      #Define index of the first point to be removed when we get a new point 
         self.global_mean = self.window.mean(axis = 0)                                          #Mean of each series contained in the sliding window
         self.global_variance = self.window.var(axis = 0) + ZERO_DIVISION_SAFE  #Variance of each series contained in the sliding window
-        self.percentils_index = np.asarray([i * (self.window_size - 1) / self.alphabet_size for i in xrange(self.alphabet_size - 1)])
+        self.percentils_index = np.asarray([i * (self.window_size - 1) / self.alphabet_size for i in xrange(1, self.alphabet_size)])
         self.znormalization()
         self.percentils = self.sorted_distribution[self.percentils_index,:]#[self.sorted_distribution[i,:] for i in self.percentils_index]
         self.subwin_means = np.asarray(map(lambda xs: xs.mean(axis = 0), np.vsplit(self.window, self.nb_subwindow)))         #Numpy array containing the mean of each subwindow 
@@ -41,14 +41,13 @@ class Dynamic_SAX:
         self.sorted_distribution = self.sorted_distribution.T.tolist()
         self.percentils = self.percentils.T.tolist()
         self.PAA_to_SAX()
-        #self.unormalization()
 
 
     def update_global_parameters(self,new_point):
         """
-            Add the new collected point and remove the oldest one in window attribute. It also updates the sliding window's features (mean, frequency and variance)
-            :param new_point : New point collected to add
-            :type new_point : Float number
+            Update all global parameters (mean value and variance of the slifing frame as well as percentils of the distribution) with the new point.
+            :param new_point : New points collected to be add.
+            :type new_point : List of float number. Must be the same dimension than the number of signals treated.
         """
         new_point = (new_point - self.global_mean) * 1.0 / self.global_variance 
         removed_point = self.window[self.index_oldest]
@@ -61,22 +60,29 @@ class Dynamic_SAX:
             sinsert(self.sorted_distribution[index], new_point[index])
         self.update_percentils()
 
+
     def update_percentils(self):
+        """
+            Update global percentils of each time serie used to perform SAX transform.
+        """
         for i,percentil in enumerate(self.percentils):
             for j,value in enumerate(percentil):
                 self.percentils[i][j] = self.sorted_distribution[i][self.percentils_index[j]] 
 
+
     def znormalization(self):
         """
-            Normalize the whole window by substracting its own mean and dividing by its own variance.
+            Normalize the whole window and the sorted distribution on every dimension by substracting the proper mean and dividing by the proper variance.
         """
         self.window = (self.window - self.global_mean) / (np.sqrt(self.global_variance)+ZERO_DIVISION_SAFE)
         self.sorted_distribution  = (self.sorted_distribution - self.global_mean) / (np.sqrt(self.global_variance)+ZERO_DIVISION_SAFE)
 
 
-    def PAA_transform(self, new_point):            #To have good quantiles we'll need to find good index for each quntile ---> ordered list splitted in alpha parts then tkae the goods one to have quantiles ! Then to update just need to delete one and search where to put the oher
+    def PAA_transform(self, new_point):            
         """
-            Perform the discretization and store the transformation into SAX attribute.
+            Perform the PAA transform on every dimension, updating the interval means with the new point and once the window has slid.
+            :param new_point : New point to add
+            :type new_point : List of float number. Must be the same dimension than the number of signals treated.
         """
         for index, mean in enumerate(self.subwin_means):
             current_oldest = self.index_oldest + self.len_subwindow * index
@@ -93,12 +99,22 @@ class Dynamic_SAX:
         if self.index_oldest == self.window_size : 
             self.index_oldest = 0
 
+
     def PAA_to_SAX(self):
+        """
+            Transform the mean value of each interval to the chosen alphabet for each dimension thanks to the proper percentils.
+        """
         for i in xrange(self.SAX.shape[0]):
             for j in xrange(self.SAX.shape[1]):
                 self.SAX[i][j] = self.alphabet[sindex(self.percentils[j],self.subwin_means[i][j])]
 
+
     def SAX_transform(self, new_point):
+        """
+            Perform a dynamic SAX transform for a new incoming captured data.
+            :param new_point : New incoming point to integrate to the time serie.
+            :type new_point : List of float number. Must be the same dimension than the number of signals treated.
+        """
         self.update_global_parameters(new_point)
         self.PAA_transform(new_point)
         self.PAA_to_SAX()
